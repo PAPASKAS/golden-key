938483ba1950cf2ec07a71265df8f18b02a797eb
PAPAPSKAS <pavel.glugxix@gmail.com>
2022-08-10 14:26:52 +0300

    ...

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,9 @@ class HomeController extends Controller
 {
     public function __invoke(): View
     {
-        return view('index', [
-            'real_estates' => RealEstate::all(),
-        ]);
+//        return view('index', [
+//            'real_estates' => RealEstate::all(),
+//        ]);
+        return view('index');
     }
 }
